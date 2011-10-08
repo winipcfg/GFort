b1c356ba418c877e4354b03902ed526c098b5957
Summoner <winipcfg@gmail.com>
2011-10-08 19:42:18 +0800

    Refine IObserver class

--- GFort/Core/IObserver.h
+++ GFort/Core/IObserver.h
@@ -1,112 +1,111 @@
-#ifndef GFORT_IOBSERVER_H_
-#define GFORT_IOBSERVER_H_
+//Copyright (C) 2011 by Gavin Fong
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
 
+#ifndef GFORT_CORE_IOBSERVER_H_
+#define GFORT_CORE_IOBSERVER_H_
+
+#include <cstdarg>
 #include <list>
 #include <string>
 
-namespace GFort
+namespace GFort { namespace Core
 {
 
-typedef std::string State;
+template <typename T> class ISubject;
 
-class IObserver;
+//=================================================================================
+/// Observer Interface
+template <typename T>
+class IObserver
+{
+public:
+    /// Constructor.
+    IObserver() {}
+
+    /// Destructor.
+    virtual ~IObserver() {}
+
+    /// Update observer.
+    /// @param subject The subject that notify the observer
+    virtual void Update(T* subject) = 0;   
+};
 
 //=================================================================================
 /// Subject Interface
+template <typename T>
 class ISubject
 {
 public:
-    /// Destructor
+    /// Destructor.
     virtual ~ISubject();
 
-    /// Attach observer to the subject
+    /// Attach observer to the subject.
     /// @param observer
-    virtual void Attach(IObserver* observer);
+    virtual void Attach(IObserver<T>* observer);
 
-    /// Detach observer from the subject
+    /// Detach observer from the subject.
     /// @param observer
-    virtual void Detach(IObserver* observer);
+    virtual void Detach(IObserver<T>* observer);
 
-    /// Notify all observers that the subject is updated
+    /// Notify all observers that the subject is updated.
     virtual void Notify();
-
-    virtual void SetState(const State& state) = 0;
-
-    virtual State GetState() const = 0;
-
+        
 protected:
-    /// Constructor
-    ISubject();
+    /// Constructor.
+    ISubject() {}
 
 private:
     // Stores list of observer
-    std::list<IObserver* > observer_list_;
+    std::list<IObserver<T>* > observer_list_;
 };
 
-void ISubject::Attach(IObserver* observer)
+template <typename T>
+inline ISubject<T>::~ISubject()
+{
+    observer_list_.clear();
+}
+
+template <typename T>
+inline void ISubject<T>::Attach(IObserver<T>* observer)
 {
     if (observer != NULL)
         observer_list_.push_back(observer);
 }
 
-void ISubject::Detach(IObserver* observer)
+template <typename T>
+inline void ISubject<T>::Detach(IObserver<T>* observer)
 {
     if (observer != NULL)
         observer_list_.remove(observer);
 }
 
-void ISubject::Notify()
+template <typename T>
+inline void ISubject<T>::Notify()
 {
-    std::list<IObserver* >::iterator it;
+    std::list<IObserver<T>* >::iterator it;
     for (it = observer_list_.begin(); it != observer_list_.end(); ++it)
     {
-        (*it)->Update(this);
+        (*it)->Update(static_cast<T*>(this));
     }
 }
 
-//=================================================================================
-/// Observer Interface
-class IObserver
-{
-public:
-    /// Constructor
-    /// @param subject The object to observe
-    IObserver(ISubject* subject);
+} } // namespace
 
-    /// Destructor
-    virtual ~IObserver();
-
-    /// Update observer
-    /// @param subject The subject that notify the observer
-    virtual void Update(ISubject* subject);
-    
-protected:
-    /// Constructor
-    IObserver();
-
-    ISubject*   subject_;
-    State       state_;
-
-private:
-
-};
-
-IObserver::IObserver(ISubject* subject)
-{
-    subject_ = subject;
-    subject_->Attach(this);
-}
-
-IObserver::~IObserver()
-{
-    subject_->Detach(this);
-}
-
-void IObserver::Update(ISubject* subject)
-{
-    state_ = subject->GetState();
-}
-
-} // namespace
-
-#endif //GFORT_IOBSERVER_H_
+#endif //GFORT_CORE_IOBSERVER_H_
